f2be6e482cb82060d03decacaad07c80fe99e743
karachevtsev <karachevtsev@yahoo.com>
2019-09-27 17:40:33 +0300

    Added cart functionality: add and remove goods, count total cart price, count goods in cart

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const filterCheckbox = document.querySelectorAll('.js-filter-checkbox');
-const btnOpenCart = document.getElementById('cart');
 const cart = document.querySelector('.js-modal-cart');
+const btnOpenCart = document.querySelector('.js-cart-open');
 const btnCloseCart = document.querySelector('.js-cart-close');
 
 filterCheckbox.forEach(function (checkbox) {
@@ -24,3 +24,50 @@ btnCloseCart.addEventListener('click', () => {
     cart.classList.remove('is-active');
     document.body.classList.remove('modal-open');
 });
+
+const catalogCards = document.querySelectorAll('#goods .card');
+const cartWrapper = document.querySelector('.js-cart-wrapper');
+const cartEmpty = document.getElementById('cart-empty');
+const cartCounter = document.querySelector('.js-cart-counter');
+const cartTotalPrice = document.querySelector('.js-cart-total');
+
+catalogCards.forEach((card) => {
+    const btnAddToCart = card.querySelector('button');
+
+    btnAddToCart.addEventListener('click', () => {
+        const cardClone = card.cloneNode(true);
+        const btnRemoveFromCart = cardClone.querySelector('.btn');
+
+        cartWrapper.appendChild(cardClone);
+        btnRemoveFromCart.textContent = 'Удалить из корзины';
+        showCartData();
+
+        btnRemoveFromCart.addEventListener('click', () => {
+            cardClone.remove();
+            showCartData()
+        });
+    });
+});
+
+function showCartData() {
+    const cartCards = cartWrapper.querySelectorAll('.card');
+    const cardsPrice = cartWrapper.querySelectorAll('.card-price');
+    let total = 0;
+
+    cartCounter.textContent = cartCards.length;
+
+    cardsPrice.forEach((cardPrice) => {
+        let price = parseFloat(cardPrice.textContent);
+        total += price;
+    });
+
+    cartTotalPrice.textContent = total;
+
+    if (cartCards.length !== 0) {
+        cartEmpty.remove();
+    } else {
+        cartWrapper.appendChild(cartEmpty);
+    }
+}
+
+
